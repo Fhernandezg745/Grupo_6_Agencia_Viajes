ffe69d6ed2fcc90ac06be854a2e6b9a0ac4687f5
Fhernandezg745 <fer.hndez@gmail.com>
2022-07-13 13:29:46 -0300

    arreglos en rutas,controllers, carpetas, users, login, register

--- web/app.js
+++ web/app.js
@@ -1,14 +1,19 @@
+//Require de librerias, frameworks, etc.
+
 const { path, resolve } = require("path");
 const express = require("express");
 const app = express();
 const { port, callback } = require("./src/modules/port");
 
+//seteo carpeta public como static
 const public = resolve(__dirname, "./public");
 app.use(express.static(public));
 
+// seteo de EJS
 app.set("view engine", "ejs");
 app.set("views", resolve(__dirname, "./src/views"));
 
+// rutas
 const mainRoutes = require("./src/routes/mainRoutes");
 const productRoutes = require("./src/routes/productRoutes");
 const usersRoutes = require("./src/routes/usersRoutes");
@@ -17,6 +22,12 @@ app.use("/user", usersRoutes);
 app.use("/", mainRoutes);
 app.use("/products", productRoutes);
 
+//
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json)
+app.use(method('m')); //poner en URL ?m=<<método-DELETE_PUT>>
+
+//levanto servidor
 app.listen(port, () =>
   console.log(`Abriendo el servidor http://localhost:${port}`)
 );

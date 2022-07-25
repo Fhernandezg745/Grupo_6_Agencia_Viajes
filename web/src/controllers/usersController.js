986f57094bcc4f2666773610e3ff4fe057573e23
dfpiscitelli <damianpiscitelli@gmail.com>
2022-07-25 18:20:30 -0300

    creando CRUD de users

--- web/src/controllers/usersController.js
+++ web/src/controllers/usersController.js
@@ -1,7 +1,25 @@
+const {
+  index,
+  one,
+  create,
+  write,
+  search,
+} = require("../models/users.model");
+
 
 const usersController =  {
     register: (req, res) => {
-      return res.render("users/register");
+      return res.render("users/register", {
+        title: "Register",
+      });
+    },
+    save: (req, res) => {
+        req.body.image = req.files[0].filename;
+        let newUser = create(req.body);
+        let user = index();
+        user.push(newUser);
+        write(user);
+        return res.redirect("/users/logged");
     },
     login: (req, res) => {
       return res.render("users/login");
--- web/src/routes/usersRoutes.js
+++ web/src/routes/usersRoutes.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
-
+const { extname } = require("path");
 const usersController = require("../controllers/usersController");
 
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/users");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix + extname(file.originalname));
+  },
+});
+const upload = multer({ storage: storage });
+
 router.get("/register", usersController.register);
+router.post("/save", [upload.any()], usersController.save);
 
 router.get("/login", usersController.login);
 

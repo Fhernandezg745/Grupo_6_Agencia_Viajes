d6e8b633b34aaf642f153acdf098134fec1ca3f6
dfpiscitelli <damianpiscitelli@gmail.com>
2022-07-26 18:30:07 -0300

    conflicto delete-CRUD-testing

--- web/src/routes/productRoutes.js
+++ web/src/routes/productRoutes.js
@@ -20,6 +20,10 @@ router.put("/editProduct/:id", [upload.any()], productsController.modify);
 
 router.get("/cart", productsController.cart);
 
-router.delete("/delete/:id", productsController.deleteProduct);
+router.delete("/products/delete/:id", function(req, res,) {
+    res.send("test");
+}); 
+
+//router.delete("/delete/:id", productsController.deleteProduct);
 
 module.exports = router;

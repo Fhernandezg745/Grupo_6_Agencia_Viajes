e1d069a2de092c35644194a4c44f4c6648e1c0db
Andrés Ibarra <andresm.ibarrag@unilibrebog.edu.co>
2022-08-24 14:48:29 -0500

    Andrés. isAdmin fixed

--- web/src/middlewares/isAdmin.js
+++ web/src/middlewares/isAdmin.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
-    if (!req.session.user.position == 'admin') {
+    if (req.session.user && req.session.user.position !== 'admin') {
         return res.redirect('/')
     }
+    
     return next()
 }

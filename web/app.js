7c700231036595193ec292182cbd0d70614f64a2
Fhernandezg745 <fer.hndez@gmail.com>
2022-09-23 17:58:52 -0300

    Fer_creo api de products y user version1

--- web/app.js
+++ web/app.js
@@ -3,13 +3,14 @@
 const { resolve } = require("path");
 const express = require("express");
 const app = express();
-const { port} = require("./src/modules/port");
+const { port } = require("./src/modules/port");
 const method = require('method-override');
 const session = require('express-session')
+const cors = require('cors');
 
 //levanto servidor
 app.listen(port, () =>
-console.log(`Abriendo el servidor http://localhost:${port}`)
+    console.log(`Abriendo el servidor http://localhost:${port}`)
 );
 
 //seteo carpeta public como static
@@ -22,18 +23,22 @@ app.set("views", resolve(__dirname, "./src/views"));
 
 
 // seteo de CRUD
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(method('_method'));
 app.use(session({
     secret: 'nodejs',
     saveUninitialized: true,
     resave: true
-})) 
-app.use(require('./src/middlewares/user'))
+}))
+app.use(cors());
+app.use(require('./src/middlewares/user'));
 // req.session
 
 // rutas
 app.use("/", require("./src/routes/mainRoutes"));
 app.use("/products", require("./src/routes/productRoutes"));
 app.use("/users", require("./src/routes/usersRoutes"));
+
+//APIS
+app.use('/api/users', require('./src/routes/apis/usersApi.routes'))

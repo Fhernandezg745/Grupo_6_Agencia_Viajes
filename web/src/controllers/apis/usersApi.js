d022a053855c38be766b4071071aab7e79e05d4d
Andrés Ibarra <andresm.ibarrag@unilibrebog.edu.co>
2022-09-28 02:32:46 -0500

    Andrés - Modifico API de usuarios para mostrar info determinada

--- web/src/controllers/apis/usersApi.js
+++ web/src/controllers/apis/usersApi.js
@@ -5,12 +5,33 @@ const { hashSync } = require('bcryptjs');
 const userApi = {
     all: async(req, res) => {
         try {
-            let users = await user.findAll({
-                include: { all: true }
+            let page = 0;
+            
+            if (req.query.offset) {
+                page = parseInt(req.query.offset);
+            }
+
+            let usersDB = await user.findAll({
+                include: { all: true },
+                limit: 10,
+                offset: page,
+                order: [['id', 'ASC']]
             });
-            return res.status(200).json(users)
+
+            let data = {};
+            data.usersTotal = await user.count();
+            data.users = usersDB.map(user => Object({
+                id : user.id,
+                url : 'http://localhost:3002/users/pending?/' + user.id, //Crear vista de perfil de usuario?
+                avatar : 'http://localhost:3002/avatars/' + user.avatar, //arreglar relación de avatar de usuario
+                name : user.name,
+                lastName : user.lastName,
+                email : user.email,
+                birthDate : user.birthDate,
+            }));
+            return res.status(200).json({ next: data.users.length> 0 ? 'http://localhost:3002/api/users?offset=' + (page + 10) : null, data });
         } catch (error) {
-            return res.status(500).json(error)
+            return res.status(500).json(error);
         }
     },
     oneUser: async(req, res) => {
@@ -19,7 +40,15 @@ const userApi = {
                 include: { all: true }
             })
             if (userDB) {
-                return res.status(200).json(userDB)
+                let data = {};
+                data.id = userDB.id;
+                data.url = 'http://localhost:3002/users/pending?/' + userDB.id;
+                data.avatar = 'http://localhost:3002/avatars/' + userDB.avatar;
+                data.name = userDB.name;
+                data.lastName = userDB.lastName;
+                data.email = userDB.email;
+                data.birthDate = userDB.birthDate.toDateString("DD/MM/YYYY");
+                return res.status(200).json(data)
             } else {
                 return res.status(404).json(userDB)
             }

d87e620c7547c0009e2e02f34e746e2c3a6b2925
Fhernandezg745 <68292451+Fhernandezg745@users.noreply.github.com>
2022-10-11 18:12:31 -0300

    Delete dashboard_ht directory

--- dashboard_ht/src/services/apiProductsHarbortrip.js
+++
@@ -1,8 +0,0 @@
-const baseURL = 'http://localhost:3002/api/products?offset='  // ??
-
-export async function getAll(page){
-    let endpoint = `${baseURL}?page=${page}`
-    let query = await fetch(endpoint)
-    let data = await query.json()
-    return data.results
-}
